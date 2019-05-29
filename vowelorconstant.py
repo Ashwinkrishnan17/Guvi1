ce0ab016a50ff4adc5822fad3bc87e6c5494f4fe
Ashwinkrishnan17 <50882865+Ashwinkrishnan17@users.noreply.github.com>
2019-05-29 13:09:46 +0530

    Update vowelorconstant.py

--- vowelorconstant.py
+++ vowelorconstant.py
@@ -1,4 +1,4 @@
-a = input("enter a value")
+a = input()
 if(a.isalpha()):
   b = "consonant"
   if(a == "a" or a == "e" or a == "i" or a == "o" or a == "u"or a == "A" or a == "E" or a == "I" or a == "O" or a == "U"):
