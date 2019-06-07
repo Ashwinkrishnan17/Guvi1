4d05992704cc3fcb07b88cf9aced97d40fcc3963
Ashwinkrishnan17 <50882865+Ashwinkrishnan17@users.noreply.github.com>
2019-06-07 22:59:20 +0530

    Update max element.py

--- max element.py
+++ max element.py
@@ -1,12 +1,6 @@
 n = int(input())
 a = input().split()
-i = 0
-if(int(a[i]) > int(a[i+1])):
-  if(int(a[i]) > int(a[i+2])):
-    print(a[i])
-  else:
-    print(a[i+2])
-elif(int(a[i+1]) > int(a[i+2])):
-  print(a[i+1])
-else:
-  print(a[i+2])
+b = []
+for i in range(0,n):
+  b.append(int(a[i]))
+print(max(b))
