a0a2e4c3bfdfbc741872b8a00b5c7c5330493012
Ashwinkrishnan17 <50882865+Ashwinkrishnan17@users.noreply.github.com>
2019-06-10 20:28:26 +0530

    Create check whether it is number exists.py

---
+++ check whether it is number exists.py
@@ -0,0 +1,11 @@
+a,b = map(int,input().split())
+c = input().split()
+list = []
+for i in c:
+  list.append(int(i))
+for j in range(0,len(c)):
+  if(list[j] == b):
+    print("yes")
+    break
+else:
+  print("no")
