92dacbed35cbe2b27bd345190988928abbc1aa65
Ashwinkrishnan17 <50882865+Ashwinkrishnan17@users.noreply.github.com>
2019-07-28 12:40:36 +0530

    Update Sum of all the numbers previously seen on the stairs which are smaller than the present number.calculate the sum of all numbers.py

--- Sum of all the numbers previously seen on the stairs which are smaller than the present number.calculate the sum of all numbers.py
+++ Sum of all the numbers previously seen on the stairs which are smaller than the present number.calculate the sum of all numbers.py
@@ -1,7 +1,10 @@
 n = int(input())
 a = list(map(int,input().split()))
 b = []
-for i in a:
-  for j in range(0,i):
+for i in range(1,len(a)):
+  c = a.index(a[i])
+  d = a[:c]
+  for j in d:
+    if(j < a[i]):
       b.append(j)
 print(sum(b))
